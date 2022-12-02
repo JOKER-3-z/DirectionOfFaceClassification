f2ac95af976440be25ac09ff5575a46227bf6158
ZY <zylalala666@163.com>
2022-12-02 22:44:07 +0800

    using dlib to recognize face site

--- Data.py
+++ Data.py
@@ -5,7 +5,38 @@ import pandas as pd
 from torchvision import transforms
 from torch.utils.data import DataLoader,Dataset
 
+## test ---22-12-02
+import os
+import cv2 as cv
+import dlib
+def getface(imgpath):
+  detector = dlib.get_frontal_face_detector()
+  img=cv.imread(imgpath)
+  #转为灰度图像寻找面部
+  img_gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+  #获取面部位置
+  face_sets=detector(img_gray,1)
+  for face_set in face_sets:
+    y1 = face.bottom()  # detect box bottom y value
+    y2 = face.top()  # top y value
+    x1 = face.left()  # left x value
+    x2 = face.right()  # right x value
+    #cv2.rectangle(img,(int(x1),int(y1)),(int(x2),int(y2)),(0,255,0),3) #添加面部框
+    img=img[min(y1,y2):max(y1,y2),min(x1,x2):max(x1,x2),:] #裁切面部
+  return img
+
+if __name__=='__main__':
+  path = "/content" #文件夹目录
+  files= os.listdir(path) #得到文件夹下的所有文件名称
+  for i in range(len(files)): #遍历文件夹
+    if not os.path.isdir(files[i]): #判断是否是文件夹，不是文件夹才打开
+      img=getface(path+"/"+files[i])
+      cv.imwrite(str(i)+".jpg",img)
+      print(str(i+1))
+
+## main
 #image是头部旋转图像，这里label是头部旋转类别
+
 class PoseDataset(Dataset):
     def __init__(self,image,label,transform=None):
         self.transform = transform
@@ -25,6 +56,7 @@ class PoseDataset(Dataset):
             # 归一化，转为torch
             image = torch.torch.tensor(image/255,dtype=torch.float)
         return image,label
+
 
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
